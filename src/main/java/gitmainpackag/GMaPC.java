eacb14f4986b11115477e88ea5d71b13cea7a07b
bsand <bsand@LAPTOP-JB2CQI02>
2022-09-01 13:39:26 +0530

    first commit

---
+++ src/main/java/gitmainpackag/GMaPC.java
@@ -0,0 +1,25 @@
+package gitmainpackag;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GMaPC {
+	public GMaPC() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.toolsqa.com/selenium-webdriver/webdrivermanager/");
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.close();
+		
+		
+	}
+	
+
+}
